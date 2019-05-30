8b2a424216c83152671751ebf4b3ef945ffd6bc0
Unknown <unknown@example.com>
2019-05-30 11:42:29 -0300

    Footer + estructura home

---
+++ ProyectoIntegrador/js/home.js
@@ -0,0 +1,25 @@
+window.onload=function(){
+   fetch("https://api.themoviedb.org/3/movie/popular?api_key=populares&language=en-US&page=1")
+   .then(function(respuesta){
+     return respuesta.json()
+   })
+   .then(function(informacion){
+       console.log(informacion);
+       var titulo=informacion.data.title
+       var url=informacion.data.poster_path
+
+       document.querySelector("carrousel").innerHTML += "<article><h2>" + titulo + "</h2><img src='" + url +  "'></article>"
+
+   })
+   .catch(function(error){
+     console.log("Error" + error)
+   })
+
+
+
+
+
+
+
+
+}
