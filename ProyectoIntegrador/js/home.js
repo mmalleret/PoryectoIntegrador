4f095843f9edaaf94f01de61b866bfeded351129
Unknown <unknown@example.com>
2019-05-31 18:46:46 -0300

    Auto stash before merge of "master" and "origin/master"
    Deje el logo de ustedes
    carrousel de peliculas

--- ProyectoIntegrador/js/home.js
+++ ProyectoIntegrador/js/home.js
@@ -46,21 +46,63 @@ window.onload = function(){
      for(i=0; i<3; i++){
      var titulo=informacion.results[i].title
      var url=informacion.results[i].poster_path
-     document.querySelector("#carrousel-populares").innerHTML += '<div id="peli'+i+'" class="carousel-item"><img src="https://image.tmdb.org/t/p/original/'+url+'" class="d-block w-100" alt=""><div class="carousel-caption d-none d-md-block"><h3>'+titulo+'</h3></div></div>'
-
      if (i == 0) {
-       document.querySelector('#peli0').classList.add('active')
+       document.querySelector("#carrousel-populares").innerHTML += '<div class="carousel-item active"><img src="https://image.tmdb.org/t/p/original/'+url+'" class="d-block w-100" alt=""><div class="carousel-caption d-none d-md-block"><h3>'+titulo+'</h3></div></div>'
+
+     } else {
+       document.querySelector("#carrousel-populares").innerHTML += '<div class="carousel-item"><img src="https://image.tmdb.org/t/p/original/'+url+'" class="d-block w-100" alt=""><div class="carousel-caption d-none d-md-block"><h3>'+titulo+'</h3></div></div>'
+
      }
-
-
    }
  })
  .catch(function(error){
    console.log("Error" + error)
  })
+//API peliculas mejores rankeadas//
+ fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=9fe1abda2acd785b6fc8d949de634904&language=en-US&page=1")
+ .then(function(respuesta){
+   return respuesta.json()
+ })
+ .then(function(informacion){
+     console.log(informacion);
+     for(i=0; i<3; i++){
+     var titulo=informacion.results[i].title
+     var url=informacion.results[i].poster_path
 
+     if (i == 0) {
+       document.querySelector("#carrousel-puntuadas").innerHTML += '<div class="carousel-item active"><img src="https://image.tmdb.org/t/p/original/'+url+'" class="d-block w-100" alt=""><div class="carousel-caption d-none d-md-block"><h3>'+titulo+'</h3></div></div>'
 
+     } else {
+       document.querySelector("#carrousel-puntuadas").innerHTML += '<div class="carousel-item"><img src="https://image.tmdb.org/t/p/original/'+url+'" class="d-block w-100" alt=""><div class="carousel-caption d-none d-md-block"><h3>'+titulo+'</h3></div></div>'
 
+     }
+   }
+ })
+ .catch(function(error){
+   console.log("Error" + error)
+ })
+ //API proximos estrenos//
+ fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=9fe1abda2acd785b6fc8d949de634904&language=en-US&page=1")
+ .then(function(respuesta){
+   return respuesta.json()
+ })
+ .then(function(informacion){
+     console.log(informacion);
+     for(i=0; i<3; i++){
+     var titulo=informacion.results[i].title
+     var url=informacion.results[i].poster_path
+     if (i == 0) {
+       document.querySelector("#carrousel-estrenos").innerHTML += '<div class="carousel-item active"><img src="https://image.tmdb.org/t/p/original/'+url+'" class="d-block w-100" alt=""><div class="carousel-caption d-none d-md-block"><h3>'+titulo+'</h3></div></div>'
+
+     } else {
+       document.querySelector("#carrousel-estrenos").innerHTML += '<div class="carousel-item"><img src="https://image.tmdb.org/t/p/original/'+url+'" class="d-block w-100" alt=""><div class="carousel-caption d-none d-md-block"><h3>'+titulo+'</h3></div></div>'
+
+     }
+   }
+ })
+ .catch(function(error){
+   console.log("Error" + error)
+ })
 
 
 
