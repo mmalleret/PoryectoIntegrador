1b306964749cfcdb3a48f1050db1a07723b37d3f
Mmalleret <milimalleret1@gmail.com>
2019-06-01 13:08:19 -0300

    botones y practica en el boton de busqueda

--- ProyectoIntegrador/js/home.js
+++ ProyectoIntegrador/js/home.js
@@ -11,31 +11,38 @@ window.onload = function(){
          document.querySelector(".alert").style.display = "none"
        },3000)
      }}
+     
      // Boton buscador
-   // var queryString = new URLSearchParams(location.search)
-   //
-   // var busco = queryString.get("#buscador")
-   //
-   // fetch("https://api.themoviedb.org/3/search/movie?api_key=9fe1abda2acd785b6fc8d949de634904&language=en-US&query="+ busco +"&page=1&include_adult=false")
-   //   .then(function(respuesta) {
-   //     return respuesta.json()
-   //   })
-   //   .then(function(informacion) {
-   //     var arrayDePelis = informacion.results
-   //
-   //     for (var i = 0; i < arrayDePelis.length; i++) {
-   //       var titulo =  arrayDePelis[i].title
-   //       var url = arrayDePelis[i].poster_path
-   //       var id = arrayDePelis[i].id
-   //       var resumen = arrayDePelis[i].backdrop_path
-   //
-   //       document.querySelector(".peliculas").innerHTML += "<p> <a href=resultados.html?idPelis=" + id + ">" + titulo + "</a></p>"
-   //       document.querySelector(".peliculas").innerHTML += "<img src=" + url + " >"
-   //     }
-   //   })
-   //   .catch(function(error) {
-   //     console.log("Error: " + error);
-   //   })
+//    var queryString = new URLSearchParams(window.location.search);
+//
+//    var buscar = queryString.get("#buscador");
+//
+//    var url = "https://api.themoviedb.org/3/search/movie?api_key=9fe1abda2acd785b6fc8d949de634904&language=en-US&query="+ buscar +"&page=1&include_adult=false"
+//
+//    fetch(url)
+//
+//      .then(function(respuesta) {
+//        return respuesta.json()
+//      })
+//      .then(function(informacion) {
+//        console.log(informacion.results);
+//        var arrayDePelis = informacion.results
+//
+//        for (var i = 0; i < arrayDePelis.length; i++) {
+//          var titulo =  arrayDePelis[i].title
+//          var url = arrayDePelis[i].poster_path
+//          var id = arrayDePelis[i].id
+//          var resumen = arrayDePelis[i].backdrop_path
+//
+//          document.querySelector(".peliculas").innerHTML += "<p> <a href=resultados.html?idPelis=" + id + ">" + titulo + "</a></p>"
+//          document.querySelector(".peliculas").innerHTML += "<img src=" + url + " >"
+//        }
+//      })
+//      .catch(function(error) {
+//        console.log("Error: " + error);
+//      })
+//      // boton buscador //
+//
  //API peliculas populares//
  fetch("https://api.themoviedb.org/3/movie/popular?api_key=9fe1abda2acd785b6fc8d949de634904&language=en-US&page=1")
  .then(function(respuesta){
--- ProyectoIntegrador/js/resultados.js
+++ ProyectoIntegrador/js/resultados.js
@@ -1,26 +1,58 @@
 window.onload = function(){
-var queryString = new URLSearchParams(location.search)
+  var queryString = new URLSearchParams(window.location.search);
 
-var busco = queryString.get("#buscador")
+     var buscar = queryString.get("#buscaPelis");
 
-fetch("https://api.themoviedb.org/3/search/movie?api_key=9fe1abda2acd785b6fc8d949de634904&language=en-US&query="+ busco +"&page=1&include_adult=false")
-  .then(function(respuesta) {
-    return respuesta.json()
-  })
-  .then(function(informacion) {
-    var arrayDePelis = informacion.results
+     var url = "https://api.themoviedb.org/3/search/movie?api_key=9fe1abda2acd785b6fc8d949de634904&language=en-US&query="+ buscar +"&page=1&include_adult=false"
 
-    for (var i = 0; i < arrayDePelis.length; i++) {
-      var titulo =  arrayDePelis[i].title
-      var url = arrayDePelis[i].poster_path
-      var id = arrayDePelis[i].id
-      var resumen = arrayDePelis[i].backdrop_path
+     fetch(url)
 
-      document.querySelector(".peliculas").innerHTML += "<p> <a href=resultados.html?idPelis=" + id + ">" + titulo + "</a></p>"
-      document.querySelector(".peliculas").innerHTML += "<img src=" + url + " >"
-    }
-  })
-  .catch(function(error) {
-    console.log("Error: " + error);
-  })
+       .then(function(respuesta) {
+         return respuesta.json()
+       })
+       .then(function(informacion) {
+         console.log(informacion.results);
+         var arrayDePelis = informacion.results
+
+         for (var i = 0; i < arrayDePelis.length; i++) {
+           var titulo =  arrayDePelis.[i].title
+           var poster = arrayDePelis.[i].poster_path
+           var id = arrayDePelis[i].id
+           var resumen = arrayDePelis[i].backdrop_path
+
+           document.querySelector(".pelis").innerHTML += "<p> <a href=resultados.html?idPelis=" + id + ">" + titulo + "</a></p>"
+           document.querySelector(".pelis").innerHTML += "<img src=" + poster + " >"
+         }
+       })
+       .catch(function(error) {
+         console.log("Error: " + error);
+       })
+       // boton buscador //
+
+
+
+// var queryString = new URLSearchParams(location.search)
+//
+// var busco = queryString.get("#buscador")
+//
+// fetch("https://api.themoviedb.org/3/search/movie?api_key=9fe1abda2acd785b6fc8d949de634904&language=en-US&query="+ busco +"&page=1&include_adult=false")
+//   .then(function(respuesta) {
+//     return respuesta.json()
+//   })
+//   .then(function(informacion) {
+//     var arrayDePelis = informacion.results
+//
+//     for (var i = 0; i < arrayDePelis.length; i++) {
+//       var titulo =  arrayDePelis[i].title
+//       var url = arrayDePelis[i].poster_path
+//       var id = arrayDePelis[i].id
+//       var resumen = arrayDePelis[i].backdrop_path
+//
+//       document.querySelector(".peliculas").innerHTML += "<p> <a href=resultados.html?idPelis=" + id + ">" + titulo + "</a></p>"
+//       document.querySelector(".peliculas").innerHTML += "<img src=" + url + " >"
+//     }
+//   })
+//   .catch(function(error) {
+//     console.log("Error: " + error);
+//   })
 }
