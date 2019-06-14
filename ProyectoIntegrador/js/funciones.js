0ab8204ae403e99a40b2c37dd08d3b4a84affccc
Mmalleret <milimalleret1@gmail.com>
2019-06-14 18:00:17 -0300

    Boton favs

--- ProyectoIntegrador/js/funciones.js
+++ ProyectoIntegrador/js/funciones.js
@@ -24,7 +24,7 @@ window.addEventListener("load", function(){
            document.querySelector(".generoDe").style.display = "none"
          }
          else {
-           document.querySelector(".generoDe").innerHTML = '<p class=idioma>'+idioma+'</p>'
+           document.querySelector(".generoDe").innerHTML = '<p class=idioma>Lenguaje original: '+idioma+'</p>'
          }
 
          document.querySelector("#elVerMas").style.display = "block";
@@ -75,66 +75,47 @@ window.addEventListener("load", function(){
 
 
 
+  //boton favoritos
+  var json = localStorage.getItem("favoritos")
+
+  // Si ya habia favoritos..
+  if (json != null) {
+  // Desempaquetar el string JSON
+  var objLit = JSON.parse(json)
+  // De todo el objeto literal me interesa EL ARRAY
+  var favoritos = objLit.carac
+  } else {
+    var favoritos = []
+      // Si no habia creo el listado como VACIO
+    }
+    for (var i = 0; i < favoritos.length; i++) {
+        var idPeli = favoritos[i]
+          fetch('"https://api.themoviedb.org/3/movie/'+ idPeli +'?api_key=9fe1abda2acd785b6fc8d949de634904&language=en-US"')
+          .then(function(respuesta) {
+                return respuesta.json()
+              })
+          .then(function(peli) {
+            pelicula = peli.results
+            var titulo =  peli.title
+            var url = peli.poster_path
+            var id = peli.id
+
+          document.querySelector("elBotonFavs").innerHTML += "<p> <a href=detalle.html?idPeli=" + id + ">" + titulo + "</a></p>"
+            // Pregunto si el gif ya era favorito
+          if (favoritos.indexOf(id) == -1) {
+            // Si no era favorito digo "Queres agregarlo?"
+            // OJO QUE COMO HAY MUCHOS BOTONES EL BOTON TIENE UNA REFERENCIA DEL idGif
+          document.querySelector("elBotonFavs").innerHTML += "<button idPeli=" + id + " class='favorito'>Agregar a Favoritos</button><br>"
+          } else {
+            // Si ya era, digo "Queres quitarlo?"
+          document.querySelector("elBotonFavs").innerHTML += "<button idPeli=" + id + " class='favorito'>Quitar de Favoritos</button><br>"
+          }
+          document.querySelector("elBotonFavs").innerHTML += "<img src=" + url + " >"
+          })
+          }
+
+
+
+
 
 })
-// //Boton de favoritos
-// var json = localStorage.getItem("favoritos")
-//
-// // Si ya habia favoritos..
-// if (json != null) {
-//   // Desempaquetar el string JSON
-//   var objLit = JSON.parse(json)
-//
-//   // De todo el objeto literal me interesa EL ARRAY
-//   var favoritos = objLit.carac
-//
-// } else {
-//   // Si no habia creo el listado como VACIO
-//   var favoritos = []
-// }
-//
-// for (var i = 0; i < favoritos.length; i++) {
-//   var idPeli = favoritos[i]
-//
-//   fetch('"https://api.themoviedb.org/3/movie/'+ idPeli +'?api_key=9fe1abda2acd785b6fc8d949de634904&language=en-US"')
-//     .then(function(respuesta) {
-//       return respuesta.json()
-//     })
-//     .then(function(peli) {
-//       pelicula = peli.results
-//       var titulo =  peli.title
-//       var url = peli.poster_path
-//       var id = peli.id
-//
-//       document.querySelector("elBotonFavs").innerHTML += "<p> <a href=detalle.html?idPeli=" + id + ">" + titulo + "</a></p>"
-//
-//       // Pregunto si el gif ya era favorito
-//       if (favoritos.indexOf(id) == -1) {
-//         // Si no era favorito digo "Queres agregarlo?"
-//         // OJO QUE COMO HAY MUCHOS BOTONES EL BOTON TIENE UNA REFERENCIA DEL idGif
-//         document.querySelector("elBotonFavs").innerHTML += "<button idGif=" + id + " class='favorito'>Agregar a Favoritos</button><br>"
-//       } else {
-//         // Si ya era, digo "Queres quitarlo?"
-//         document.querySelector("elBotonFavs").innerHTML += "<button idGif=" + id + " class='favorito'>Quitar de Favoritos</button><br>"
-//       }
-//
-//       document.querySelector("elBotonFavs").innerHTML += "<img src=" + url + " >"
-//     })
-// }
-//
-//
-// function botonMasMenos ( ){
-// //Boton de ver mas y ver menos
-//  document.querySelector("#vamos").onclick = function(e){
-//    document.querySelector("#elVerMas").classList.toggle("display-none")
-//    console.log(document.querySelector("#vamos").innerText);
-//
-// if (document.querySelector("#vamos").innerText == "VER MAS") {
-//   document.querySelector("#vamos").innerHTML = 'Ver Menos'
-//
-// } else {
-//   document.querySelector("#vamos").innerText = 'Ver mas'
-//    }
-//   }
-//  }
-// }
