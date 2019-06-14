f94994110edc2372dc4e9ecd9e4253953ab76bfd
Mmalleret <milimalleret1@gmail.com>
2019-06-14 14:04:48 -0300

    cambio de nombres

--- ProyectoIntegrador/js/funciones.js
+++ ProyectoIntegrador/js/funciones.js
@@ -1,22 +1,22 @@
-// window.onload=function(){
-  //validando los datos de login//
-      // var login = document.querySelector(".formulario-login");
-      // var nombre = document.querySelector ("input[name="inputName"]");
-      // var email = document.querySelector("input[name="inpuEmail"]");
-      // login.onsubmit = function (event){
-      //   if (nombre.value ==""){
-      //     event.preventDefault();
-      //   }
-      // }
+window.onload=function(){
+  validando los datos de login//
+      var login = document.querySelector(".formulario-login");
+      var nombre = document.querySelector ("input[name="inputName"]");
+      var email = document.querySelector("input[name="inpuEmail"]");
+      login.onsubmit = function (event){
+        if (nombre.value ==""){
+          event.preventDefault();
+        }
+      }
 
-// //guardar los datos en el local storage//
-//     var nom= document.getElementById("#exampleInputName").value;
-//     var nombre = localStorage.getItem("nombre", nom);
-//
-// //mostrar los datos obtenidos//
-//     var nombre = localStorage.getItem ("Nombre");
-//     document.getElementById("#exampleInputName").innerHTML = "Bienvenido" + nombre
-//
+//guardar los datos en el local storage//
+    var usuario = document.getElementById("#exampleInputName").value;
+    var datos = localStorage.getItem("nombre", usuario);
+
+//mostrar los datos obtenidos//
+    var nombre = localStorage.getItem ("Nombre");
+    document.getElementById("#exampleInputName").innerHTML = "Bienvenido" + nombre
+
 // //Boton de favoritos
 // var json = localStorage.getItem("favoritos")
 //
